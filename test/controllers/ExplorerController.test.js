3f6db6f03d59d0d9b934455b65849c909e9a6bb1
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-27 16:18:20 -0500

    Se corrigio el codigo con linter

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,23 +1,23 @@
-const ExplorerController = require('./../../lib/controllers/ExplorerController')
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
-describe('Suite de tests para el controlador ExplorerController', () => {
+describe("Suite de tests para el controlador ExplorerController", () => {
 
-    test('Validando el metodo estatico getExplorersByMission', () => {
-        const result = ExplorerController.getExplorersByMission('node');
+    test("Validando el metodo estatico getExplorersByMission", () => {
+        const result = ExplorerController.getExplorersByMission("node");
         expect(result).not.toBeUndefined();
     });
 
-    test('Validando el metodo estatico static getExplorersUsernamesByMission', () => {
+    test("Validando el metodo estatico static getExplorersUsernamesByMission", () => {
 
-        const result = ExplorerController.getExplorersUsernamesByMission('node');
+        const result = ExplorerController.getExplorersUsernamesByMission("node");
         expect(result).not.toBeUndefined();
         expect(result.length).toBe(10);
     });
 
-    test('Validando getExplorersAmonutByMission return 10 para node', () => {
-        const result = ExplorerController.getExplorersAmonutByMission('node');
+    test("Validando getExplorersAmonutByMission return 10 para node", () => {
+        const result = ExplorerController.getExplorersAmonutByMission("node");
         expect(result).toBe(10);
-    })
+    });
 
 
-})
+});
