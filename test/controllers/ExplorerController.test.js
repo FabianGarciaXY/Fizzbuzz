7ceca233b6224d9e0ec29da834959f2392e1d1d9
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-27 23:19:53 -0500

    Se agregaron y pasaron los tests para el metodo applyValidationInNumber

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -8,7 +8,6 @@ describe("Suite de tests para el controlador ExplorerController", () => {
     });
 
     test("Validando el metodo estatico static getExplorersUsernamesByMission", () => {
-
         const result = ExplorerController.getExplorersUsernamesByMission("node");
         expect(result).not.toBeUndefined();
         expect(result.length).toBe(10);
@@ -19,5 +18,18 @@ describe("Suite de tests para el controlador ExplorerController", () => {
         expect(result).toBe(10);
     });
 
+    // Tests para la el metodo applyValidationInNumber del controlodor ExplorerController
+    test("Validando applyValidationInNumber para que retorne los valores correspondientes", () => {
+        const value1 = ExplorerController.applyValidationInNumber(1);
+        expect(value1).toBe(1);
 
+        const value3 = ExplorerController.applyValidationInNumber(3);
+        expect(value3).toBe("FIZZ");
+
+        const value5 = ExplorerController.applyValidationInNumber(5);
+        expect(value5).toBe("BUZZ");
+
+        const value15 = ExplorerController.applyValidationInNumber(15);
+        expect(value15).toBe("FIZZBUZZ");
+    })
 });
